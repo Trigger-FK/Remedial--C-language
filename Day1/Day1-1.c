1c73d06aa6c20c57244ce819c6c2a008a54ee760
Trriger_FK <fumiya99217mattyaki@gmail.com>
2022-10-30 00:45:18 +0900

    C言語リメディカル 1日目

---
+++ Day1/Day1-1.c
@@ -0,0 +1,5 @@
+#include <stdio.h> //standard input/output
+
+int main(void){
+    printf("Hello, world!\n");
+}
---
+++ Day1/Day1-2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("123\n456\n789\n");
+    printf("シングルクオーテーション：\'ダブルクオーテーション：\"\n");
+    printf("\t円マーク\\\n");
+}
