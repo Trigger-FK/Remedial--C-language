2ef73c18c00a43fcd203ba4ad6c3846b03b205f5
Trriger_FK <fumiya99217mattyaki@gmail.com>
2022-10-30 12:09:37 +0900

    Practice Day1

---
+++ Pra_No1/pra2-1.c
@@ -0,0 +1,14 @@
+
+void main(){
+    int a, b;
+    printf("a=");
+    scanf("%d", &a);
+    printf("b=");
+    scanf("%d", &b);
+    
+    printf("a + b = %d\n", a+b);
+    printf("a - b = %d\n", a-b);
+    printf("a * b = %d\n", a*b);
+    printf("a / b = %d\n", a/b);
+    printf("a %% b = %d\n", a%b);
+}
